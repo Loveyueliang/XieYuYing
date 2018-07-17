035b82613de0d173ea299fb1111155f8b110e8da
Administrator <yueliangxiaxiangyu@163.com>
2018-07-17 15:47:31 +0800

    test 测试版本,后面需要什么再讨论补充

--- Npc.py
+++ Npc.py
@@ -1,10 +1,17 @@
 #coding:utf-8
 class NPC():
+    #实现一个根据剧情调用相应的对话,参数传剧情代码
     def __init__(self):
+
         pass
 
 class XieYuYing(NPC):
+
     def __init__(self,enthusiasm):
+        '''
+
+        :param enthusiasm:好感度
+        '''
         NPC.__init__(self)
         self.name = '谢雨莹'
         self.enthusiasm = enthusiasm
