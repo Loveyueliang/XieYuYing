a84412a324bcbe84647ef90af0811efe0ccd0102
Administrator <yueliangxiaxiangyu@163.com>
2018-07-17 16:13:01 +0800

    test 测试版本,后面需要什么再讨论补充

--- Npc.py
+++ Npc.py
@@ -1,12 +1,23 @@
 #coding:utf-8
 class NPC():
-    #实现一个根据剧情调用相应的对话,参数传剧情代码
+    #实现一个根据剧情调用相应的对话,参数传剧情代码,类似1-11这种
     def __init__(self):
-
+        self.name = ''
         pass
+    def invoking_database(self):
+        '''
+        :param talk_data:调用数据库的对话存储在该属性里
+        :return:
+        '''
+        self.talk_data = '测试文本'
+        return self.talk_data
+    def __str__(self):
+        '''
+        :return:返回值为需要获取的人物属性信息 比如name,mood
+        '''
+        return 'Student object (name: %s)' % self.name
 
 class XieYuYing(NPC):
-
     def __init__(self,enthusiasm):
         '''
 
@@ -16,3 +27,6 @@ class XieYuYing(NPC):
         self.name = '谢雨莹'
         self.enthusiasm = enthusiasm
 
+
+xie = XieYuYing(80)
+print(xie)
